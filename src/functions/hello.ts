4d2e80172f6e5cded13dc102dd47b0d48351804c
William Estrada <will@futuriza.me>
2023-09-25 16:22:15 -0500

    changed esbuild format to cjs

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,12 +1,14 @@
 import { tryFn } from "../utilities/tryFn";
-import axios from 'axios'
+import axios from "axios";
 
 export const handler = async () => {
   const [ok, name] = await tryFn(async () => {
-    const { data } = await axios.get<{ name: string }>("https://rickandmortyapi.com/api/character/2")
+    const { data } = await axios.get<{ name: string }>(
+      "https://rickandmortyapi.com/api/character/2",
+    );
 
-    return data.name
-  })
+    return data.name;
+  });
 
   return {
     message: ok ? name : "NOONE",
