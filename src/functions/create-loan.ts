38f1cbbe2c07370f04a1b93e9453787e925492c4
William Estrada <will@futuriza.me>
2023-09-27 01:49:57 -0500

    added headers middleware

--- src/functions/create-loan.ts
+++ src/functions/create-loan.ts
@@ -1,7 +1,14 @@
-import { eventLog, httpError, middy } from "../middleware/index.js";
+import {
+  eventLog,
+  httpError,
+  httpHeaders,
+  middy,
+} from "../middleware/index.js";
 import { createLoanHandler } from "../adapter/input/create-loan-handler.js";
+import { HEADERS } from "../utilities/constants.js";
 
 export const handler = middy(createLoanHandler)
   .use(eventLog())
+  .use(httpHeaders(HEADERS.Cors, HEADERS.Json))
   .use(httpError())
   .start();
