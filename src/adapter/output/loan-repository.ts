a8ec7b70405c4ce6178481b26b9d260c0e08fbb3
William Estrada <will@futuriza.me>
2023-09-27 16:44:19 -0500

    added lib dynamodb

--- src/adapter/output/loan-repository.ts
+++ src/adapter/output/loan-repository.ts
@@ -1,14 +1,24 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
 import { LoanRepository } from "../../domain/loan/ports.js";
+import { Loan } from "../../domain/loan/types.js";
 
 export const loanRepository = (): LoanRepository => ({
-  createLoan: ({ id, name, amount, startAt, count, type }) => {
-    return Promise.resolve({
-      id,
-      name,
-      amount,
-      startAt,
-      count,
-      type,
+  createLoan: async ({ id, name, amount, startAt, count, type }) => {
+    const client = new DynamoDBClient();
+    const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+    const item: Loan = { id, name, amount, startAt, count, type };
+
+    const command = new PutCommand({
+      TableName: "loans",
+      Item: item,
     });
+
+    const result = await ddbDocClient.send(command);
+
+    console.log("createLoan | ", JSON.stringify(result, null, 2));
+
+    return item;
   },
 });
