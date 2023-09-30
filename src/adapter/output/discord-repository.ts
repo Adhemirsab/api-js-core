d89e8acbdae2f4a2fcde65b222e113499a7c4d8f
William Estrada <will@futuriza.me>
2023-09-29 20:40:04 -0500

    fixed catching error in discord repository

--- src/adapter/output/discord-repository.ts
+++ src/adapter/output/discord-repository.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { DiscordRepository } from "../../domain/notification/ports.js";
+import { tryFn } from "../../domain/lib/try-fn.js";
 
 export const discordRepository = (): DiscordRepository => ({
   sendDiscord: async (message) => {
@@ -8,17 +9,21 @@ export const discordRepository = (): DiscordRepository => ({
     const token =
       "NzkwMDU4OTU5NzEyNTUwOTUz.GdIo9y.OsQroib911ckDfWFb0NAST7ZWas-2UxJq6FR8c";
 
-    await axios.post(
-      `https://discord.com/api/channels/${channelId}/messages`,
-      { content: message },
-      {
-        headers: {
-          Authorization: `Bot ${token}`,
-          "Content-Type": "application/json",
+    const result = await tryFn(async () => {
+      await axios.post(
+        `https://discord.com/api/channels/${channelId}/messages`,
+        { content: message },
+        {
+          headers: {
+            Authorization: `Bot ${token}`,
+            "Content-Type": "application/json",
+          },
         },
-      },
-    );
+      );
 
-    return [true, true, undefined];
+      return true;
+    });
+
+    return result;
   },
 });
