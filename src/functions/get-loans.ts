0ba5f00daa898268a006b4ecf76944faa4767899
William Estrada <will@futuriza.me>
2023-10-30 18:25:21 -0500

    fixed issues with prettier

--- src/functions/get-loans.ts
+++ src/functions/get-loans.ts
@@ -1,15 +1,14 @@
 import {
-    eventLog,
-    httpError,
-    httpHeaders,
-    middy,
-  } from "../middleware/index.js";
-  import { getLoansHandler } from "../adapter/input/get-loans-handler.js";
-  import { HEADERS } from "../utilities/constants.js";
-  
-  export const handler = middy(getLoansHandler)
-    .use(eventLog())
-    .use(httpHeaders(HEADERS.Json))
-    .use(httpError())
-    .start();
-  
+  eventLog,
+  httpError,
+  httpHeaders,
+  middy,
+} from "../middleware/index.js";
+import { getLoansHandler } from "../adapter/input/get-loans-handler.js";
+import { HEADERS } from "../utilities/constants.js";
+
+export const handler = middy(getLoansHandler)
+  .use(eventLog())
+  .use(httpHeaders(HEADERS.Json))
+  .use(httpError())
+  .start();
